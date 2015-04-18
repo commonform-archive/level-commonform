db845c4eb42720bf26c61f52aa461897fe934367
Kyle Mitchell <kyle@kemitchell.com>
2015-04-18 22:34:37 +0000

    format tests

--- test/real-world-example.test.js
+++ test/real-world-example.test.js
@@ -12,40 +12,42 @@ describe('Real-World Example', function() {
   beforeEach(function(done) {
     var library = this.library = new Library(levelup({db: memdown}));
     var writeStream = library.createFormsWriteStream();
-    writeStream.write(example);
-    writeStream.end(done);
+    writeStream.end(example, done);
   });
 
   it('defines "Licensor"', function(done) {
     this.library.createFormsReadStream({
       predicate: 'defines',
       object: 'Licensor'
-    }).pipe(concat(function(data) {
-      expect(data).to.be.an('array');
-      expect(data).to.have.length(3);
-      done();
-    }));
+    })
+      .pipe(concat(function(data) {
+        expect(data).to.be.an('array');
+        expect(data).to.have.length(3);
+        done();
+      }));
   });
 
   it('has "Grant of Patent License"', function(done) {
     this.library.createFormsReadStream({
       predicate: 'utilizes',
       object: 'Grant of Patent License'
-    }).pipe(concat(function(data) {
-      expect(data).to.be.an('array');
-      expect(data).to.have.length(1);
-      done();
-    }));
+    })
+      .pipe(concat(function(data) {
+        expect(data).to.be.an('array');
+        expect(data).to.have.length(1);
+        done();
+      }));
   });
 
   it('implements "Grant of Patent License"', function(done) {
     this.library.createFormsReadStream({
       predicate: 'implements',
       object: 'Grant of Patent License'
-    }).pipe(concat(function(data) {
-      expect(data).to.be.an('array');
-      expect(data).to.have.length(1);
-      done();
-    }));
+    })
+      .pipe(concat(function(data) {
+        expect(data).to.be.an('array');
+        expect(data).to.have.length(1);
+        done();
+      }));
   });
 });
