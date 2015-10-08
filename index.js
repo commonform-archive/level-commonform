f968e72cca848c795a67070a173a38fee94d4504
Kyle Mitchell <kyle@kemitchell.com>
2015-10-07 21:28:42 -0700

    Store child forms

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 module.exports = LevelCommonForm
 
 var asap = require('asap')
-var validate = require('commonform-validate')
+var isChild = require('commonform-predicate').child
 var merkleize = require('commonform-merkleize')
+var validate = require('commonform-validate')
 
 function LevelCommonForm(levelup) {
   if (!(this instanceof LevelCommonForm)) {
@@ -14,10 +15,22 @@ var prototype = LevelCommonForm.prototype
 prototype.getForm = function(digest, callback) {
   this.levelup.get(digest, function(error, json) {
     if (error) {
-      callback(error) }
+      if (error.notFound) {
+        callback() }
+      else {
+        callback(error) } }
     else {
       var form = JSON.parse(json)
       callback(null, form) } }) }
+
+function addToBatch(batch, form, merkle) {
+  var json = JSON.stringify(form)
+  batch.put(merkle.digest, json)
+  form.content.forEach(function(element, index) {
+    if (isChild(element)) {
+      var childForm = element.form
+      var childMerkle = merkle.content[index]
+      addToBatch(batch, childForm, childMerkle) } }) }
 
 prototype.putForm = function(form, callback) {
   var valid = validate.form(form)
@@ -26,13 +39,14 @@ prototype.putForm = function(form, callback) {
       callback(new Error('Invalid form')) }) }
   else {
     var merkle = merkleize(form)
-    var digest = merkle.digest
-    var json = JSON.stringify(form)
-    this.levelup.put(digest, json, function(error) {
+    var root = merkle.digest
+    var batch = this.levelup.batch()
+    addToBatch(batch, form, merkle)
+    batch.write(function(error) {
       if (error) {
         callback(error) }
       else {
-        callback(null, digest) } }) } }
+        callback(null, root) } }) } }
 
 prototype.createDigestStream = function() {
   return this.levelup.createKeyStream() }
--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 var levelup = require('levelup')
 var lvf = require('.')
 var memdown = require('memdown')
+var merkleize = require('commonform-merkleize')
 var tape = require('tape')
 
 function createTestStore() {
-  return lvf(levelup('.', { db: memdown })) }
+  return lvf(levelup({ db: memdown })) }
 
 tape('put an invalid form', function(test) {
   test.plan(1)
@@ -35,3 +36,15 @@ tape('put a form and stream digests', function(test) {
         digests.push(digest) })
       .on('end', function() {
         test.deepEqual(digests, [ digest ], 'digest in list') }) }) })
+
+tape('put a form and get its child', function(test) {
+  test.plan(3)
+  var level = createTestStore()
+  var child = { content: [ 'This is a test' ] }
+  var parent = { content: [ { form: child } ] }
+  var childDigest = merkleize(child).digest
+  level.putForm(parent, function(error) {
+    test.ifError(error, 'no error on put')
+    level.getForm(childDigest, function(error, got) {
+      test.ifError(error, 'no error on get')
+      test.deepEqual(got, child, 'get the child back') }) }) })
